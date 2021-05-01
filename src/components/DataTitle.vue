490aff05bb219a418a0755a2e301d60222885eca
Robson Vinicius <robsondgn@outlook.com>
2021-05-01 19:25:35 -0300

    Título para a Data

--- src/components/DataTitle.vue
+++ src/components/DataTitle.vue
@@ -2,6 +2,7 @@
   <div class="text-center">
     <h2 class="text-3xl font-bold">{{text}}</h2>
     <div class="text-2xl mt-4 mb-10">
+      Data da Atualização: <br> 
       {{ timestamp }}
     </div>
   </div>
